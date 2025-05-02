f4478f52e0b106cdee2dc38efcca689b9669e7a0
RGRMdesign <166487037+RGRMdesign@users.noreply.github.com>
2025-05-02 14:50:20 +0000

    Release v0.0.7

--- assets/index-BVzOo81h.js
+++ assets/index-BVzOo81h.js
@@ -6,7 +6,7 @@
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${n}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript Test</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
